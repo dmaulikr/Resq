bd99db20bdd790e7e2bc232a49e048fa87fc9646
Muhammad Ahsan <ahsan386fast@gmail.com>
2016-11-15 19:28:47 +0500

    initial Release.

---
+++ Resq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Resq
+//
+//  Created by Muhammad Ahsan on 11/14/16.
+//  Copyright © 2016 Eden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Resq/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Resq
+//
+//  Created by Muhammad Ahsan on 11/14/16.
+//  Copyright © 2016 Eden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
